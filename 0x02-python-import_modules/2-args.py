3cee3014bb7b529d0854c0442aa3faa3d87510e4
Hamza <zerdoumi.hamza.um5@gmail.com>
2024-09-21 11:40:44 +0200

    python command line argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+argument = sys.argv
+size_argument = len(argument)
+
+if size_argument == 1:
+    print("{} arguments.".format(size_argument - 1))
+
+elif size_argument == 2:
+    print("{} argument:".format(size_argument - 1))
+    print("{}: {}".format(size_argument - 1, argument[1]))
+
+else:
+    print("{} arguments:".format(size_argument - 1))
+    for i in range(1, size_argument):
+        print("{}: {}".format(i, argument[i]))
