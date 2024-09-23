f0733f4f301e993cc7fd3d1204e0855eed4bc297
Hamza <zerdoumi.hamza.um5@gmail.com>
2024-09-23 23:48:25 +0200

    profession speacks now

--- 0x02-python-import_modules/7-add_tuple.py
+++ 0x02-python-import_modules/7-add_tuple.py
@@ -2,40 +2,15 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    if len(tuple_a) < 2 or len(tuple_b) < 2:
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
 
-        if len(tuple_a) == 0 and len(tuple_b) != 0:
+    return (a[0]+b[0], a[1]+b[1])
 
-            a = 0 + tuple_b[0]
-            if len(tuple_b) >= 2:
-                b = tuple_b[1]
-            else:
-                b = 0
-        if len(tuple_a) != 0 and len(tuple_b) != 0:
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
 
-            a = tuple_a[0] + tuple_b[0]
-            if len(tuple_a) == 1 and len(tuple_b) == 1:
-                b = 0
-            elif len(tuple_a) >= 2:
-
-                b = tuple_a[1]
-            elif len(tuple_b) >= 2:
-                b = tuple_b[1]
-            else:
-                b = 0
-
-        if len(tuple_a) == 0 and len(tuple_b) == 0:
-
-            a, b = 0, 0
-        if len(tuple_a) != 0 and len(tuple_b) == 0:
-
-            a = tuple_a[0] + 0
-            if len(tuple_a) >= 2:
-                b = tuple_a[1]
-            else:
-                b = 0
-    else:
-        a = tuple_a[0] + tuple_b[0]
-        b = tuple_a[1] + tuple_b[1]
-
-    return (a, b)
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
